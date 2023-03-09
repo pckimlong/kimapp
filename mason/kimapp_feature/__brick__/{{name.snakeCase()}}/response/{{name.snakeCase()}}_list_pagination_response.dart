41866df35ded15d2018ec438a928e369f113eeb0
Kim <78638472+pckimlong@users.noreply.github.com>
2023-03-09 19:56:22 +0700

    response

---
+++ mason/kimapp_feature/__brick__/{{name.snakeCase()}}/response/{{name.snakeCase()}}_list_pagination_response.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part "{{name.snakeCase()}}_list_pagination_response.freezed.dart";
+
+@freezed
+class {{name.pascalCase()}}ListPaginationResponse with _${{name.pascalCase()}}ListPaginationResponse {
+  const {{name.pascalCase()}}ListPaginationResponse._();
+
+  factory {{name.pascalCase()}}ListPaginationResponse({
+    required List<{{name.pascalCase()}}Model> items,
+    required int count,
+  }) = _{{name.pascalCase()}}ListPaginationResponse;
+}
