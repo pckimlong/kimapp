890964a5fe0027758d6dad6451198d1d2d5414ce
Kim <78638472+pckimlong@users.noreply.github.com>
2023-03-10 23:34:10 +0700

    failure

--- packages/kimapp_supabase_helper/lib/failure.dart
+++ packages/kimapp_supabase_helper/lib/failure.dart
@@ -7,91 +7,92 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 final logger = Logger();
 
-
-void _logError(String title, String message, StackTrace stackTrace, Object e){
+void _logError(String title, String message, StackTrace stackTrace, Object e) {
   final loggerCallback = Kimapp.instance.logger;
-  if(loggerCallback != null){
+  if (loggerCallback != null) {
     loggerCallback(
       type: LoggerType.error,
       title: title,
       message: message,
       stackTrace: stackTrace,
+      errorObject: e,
     );
   }
 }
-
 
 Future<Either<Failure, T>> errorHandler<T>(FutureOr<Either<Failure, T>> Function() callback) async {
   try {
     return await callback();
   } on Failure catch (e) {
-    _logError('Failure', e.message(), e.stackTrace);
+    _logError('Failure', e.message(), e.stackTrace, e);
     return left(e);
   } on AuthException catch (e, str) {
-    _logError('Supabase.AuthException', e.message, str);
+    _logError('Supabase.AuthException', e.message, str, e);
 
     if (e.message == "Invalid login credentials") {
-      return left(
-        Failure.authFailure(AuthFailures.incorrectLoginCredential(FailureInfo(stackTrace: str, debugMessage: e.message)))
-      );
+      return left(Failure.authFailure(AuthFailures.incorrectLoginCredential(
+          FailureInfo(stackTrace: str, debugMessage: e.message, errorObject: e))));
     }
 
     if (e.message == "A user with this email address has already been registered") {
-       return left(
-        Failure.authFailure(AuthFailures.alreadyRegistered(FailureInfo(stackTrace: str, debugMessage: e.message)))
-      );
+      return left(Failure.authFailure(AuthFailures.alreadyRegistered(
+          FailureInfo(stackTrace: str, debugMessage: e.message, errorObject: e))));
     }
 
-    return left(Failure.authFailure(AuthFailures(FailureInfo(stackTrace: str, debugMessage: e.message))));
-
+    return left(Failure.authFailure(
+        AuthFailures(FailureInfo(stackTrace: str, debugMessage: e.message, errorObject: e))));
   } on StorageException catch (e, str) {
-    logger.("PostgrestException:", error: e);
-    _reportError(e, str);
-    
+    _logError('Supabase.StorageException', e.message, str, e);
 
-    return left(Failure.databaseError(str, e.message));
+    return left(
+      Failure.databaseFailure(
+        DatabaseFailures(
+          FailureInfo(stackTrace: str, debugMessage: e.message, errorObject: e),
+        ),
+      ),
+    );
   } on PostgrestException catch (e, str) {
-    _reportError(e, str);
-    
-    logger.("PostgrestException:", error: e);
+    _logError('Supabase.StorageException', e.message, str, e);
 
     if (e.code == "23505") {
-      return left(Failure.uniqueConstraint(str, kDebugMode ? e.message : 'ទិន្នន័យស្ទួន'));
+      return left(Failure.databaseFailure(DatabaseFailures.uniqueConstraint(
+          FailureInfo(stackTrace: str, debugMessage: e.message, errorObject: e))));
     }
 
     if (e.code == "42P01") {
-      return left(Failure.notFound(str, e.message));
+      return left(Failure.databaseFailure(DatabaseFailures.notFound(
+          FailureInfo(stackTrace: str, debugMessage: e.message, errorObject: e))));
     }
 
     if (e.code == "42501") {
-      return left(Failure.forbidden(str, kDebugMode ? e.message : 'គ្មានសិទ្ធអនុញ្ញាត'));
+      return left(Failure.authFailure(AuthFailures.forbidden(
+          FailureInfo(stackTrace: str, debugMessage: e.message, errorObject: e))));
     }
 
     if (e.code == '502' && e.details == "Bad Gateway") {
-      return left(Failure.serverDown(str, 'មិនអាចភ្ជាប់ទៅម៉ាសុីនមេ'));
+      return left(Failure.serverError(
+          FailureInfo(stackTrace: str, debugMessage: e.message, errorObject: e)));
     }
 
-    if (e.code == "PGRST301" || e.message.contains('expired')) {
-      Supabase.instance.client.auth.refreshSession();
-    }
-
-    return left(Failure.databaseError(str, e.message));
+    return left(Failure.databaseFailure(
+        DatabaseFailures(FailureInfo(stackTrace: str, debugMessage: e.message, errorObject: e))));
   } on Exception catch (e, str) {
-    _reportError(e, str);
-    
-    logger.("Exception:", error: e);
+    _logError('Exception', e.toString(), str, e);
 
     // No internet
     if (e.toString().contains('Failed host lookup')) {
-      return left(Failure.noInternet(str, "មិនអាចភ្ជាប់ទៅបណ្ដាញអុីនធឺណេត"));
+      return left(Failure.networkFailure(
+          FailureInfo(stackTrace: str, debugMessage: e.toString(), errorObject: e)));
     }
 
-    return left(Failure.exception(str, e.toString()));
+    return left(
+      Failure.exception(FailureInfo(stackTrace: str, debugMessage: e.toString(), errorObject: e)),
+    );
   } catch (e, str) {
-    _reportError(e, str);
-    
-    logger.("Error:", error: e);
-    return left(Failure.exception(str, e.toString()));
-    // rethrow;
+    _logError('Error Catch', e.toString(), str, e);
+
+    return left(
+      Failure.exception(FailureInfo(stackTrace: str, debugMessage: e.toString(), errorObject: e)),
+    );
   }
 }
