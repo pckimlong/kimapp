63bc38ef9f2e0ade91c1cd060cef3e41b5ba4305
Kim <78638472+pckimlong@users.noreply.github.com>
2023-03-11 11:06:05 +0700

    add local storage object

--- packages/kimapp/lib/kimapp.dart
+++ packages/kimapp/lib/kimapp.dart
@@ -4,3 +4,4 @@ export 'object/object.dart';
 export 'annotation.dart';
 export 'service.dart';
 export 'riverpod_helper.dart';
+export 'local_storage.dart';
---
+++ packages/kimapp/lib/local_storage.dart
@@ -0,0 +1,44 @@
+abstract class LocalStorage {
+  LocalStorage({
+    required this.initialize,
+    required this.clear,
+    required this.save,
+    required this.read,
+    required this.saveMap,
+    required this.readMap,
+    required this.saveObject,
+    required this.readObject,
+    required this.saveString,
+    required this.readString,
+    required this.saveInt,
+    required this.readInt,
+    required this.saveEnum,
+    required this.readEnum,
+  });
+
+  final Future<void> Function() initialize;
+
+  final Future<void> Function(String key, dynamic value) save;
+  final Future<dynamic> Function(String key) read;
+
+  final Future<void> Function(String key, {required String? value}) saveString;
+  final Future<String?> Function(String key) readString;
+
+  final Future<void> Function(String key, {required int? value}) saveInt;
+  final Future<int?> Function(String key) readInt;
+
+  final Future<void> Function(String key, {required Map<String, dynamic>? value}) saveMap;
+  final Future<Map<String, dynamic>?> Function(String key) readMap;
+
+  final Future<void> Function<T extends Enum>(String key, {required T? value}) saveEnum;
+  final Future<T?> Function<T extends Enum>(String key) readEnum;
+
+  final Future<void> Function<T extends Object>(String key,
+      {required T? value, required Map<String, dynamic> Function(T object) toMap}) saveObject;
+
+  final Future<T?> Function<T extends Object>(String key,
+      {required T Function(Map<String, dynamic> json) fromMap,
+      void Function(Map<String, dynamic> json)? onFail}) readObject;
+
+  final Future<void> Function(String key) clear;
+}
