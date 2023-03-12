0920cd474dca43e4edde0271cf2c1d457a302281
Kim <78638472+pckimlong@users.noreply.github.com>
2023-03-13 01:16:23 +0700

    throw failure

--- packages/kimapp/lib/extensions.dart
+++ packages/kimapp/lib/extensions.dart
@@ -3,7 +3,7 @@ import 'package:fpdart/fpdart.dart';
 import 'kimapp.dart';
 
 extension EitherX<T> on Either<Failure, T> {
-  T getOrThrow() => getRight().getOrCrash();
+  T getOrThrow() => getOrElse((l) => throw l);
   T? getOrNull() => fold((l) => null, id);
   N mapOrThrow<N>(N Function(T r) map) => fold((l) => throw l, map);
 }
