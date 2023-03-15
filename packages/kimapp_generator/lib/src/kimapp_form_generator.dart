bc76640011589db4c037e8bb79a1d019b7314234
Kim <78638472+pckimlong@users.noreply.github.com>
2023-03-15 19:05:42 +0700

    test

---
+++ packages/kimapp_generator/lib/src/kimapp_form_generator.dart
@@ -0,0 +1,36 @@
+import 'package:analyzer/dart/element/element.dart';
+import 'package:build/build.dart';
+import 'package:kimapp/kimapp.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:source_gen/source_gen.dart';
+// ignore: depend_on_referenced_packages
+import 'package:collection/collection.dart';
+
+const providerStatusClassType = TypeChecker.fromRuntime(ProviderStatusClassMixin);
+
+class KimappFormGenerator extends GeneratorForAnnotation<Riverpod> {
+  @override
+  generateForAnnotatedElement(
+    Element element,
+    ConstantReader annotation,
+    BuildStep buildStep,
+  ) {
+    if (element is! ClassElement || !providerStatusClassType.isSuperOf(element)) {
+      /// Only support stateful provider which implement ProviderStatusClassMixin
+      return;
+    }
+
+    // Get class name
+    final className = element.thisType.toString();
+
+    // Get build method
+    final buildMethod = element.methods.firstWhereOrNull((method) => method.name == "build");
+    if (buildMethod == null) {
+      // If stateful provider has no build method, It mean it invalid
+      return;
+    }
+
+    //
+    return "$className $buildMethod";
+  }
+}
