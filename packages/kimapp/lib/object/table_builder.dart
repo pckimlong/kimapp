01246c7010c7d06c68fb0c4a0a44bcdbb1602d3f
Kim <78638472+pckimlong@users.noreply.github.com>
2023-03-21 18:27:17 +0700

    join column with candidate key must include tb

--- packages/kimapp/lib/object/table_builder.dart
+++ packages/kimapp/lib/object/table_builder.dart
@@ -30,7 +30,7 @@ class ColumnBuilder with _$ColumnBuilder {
     return map(
       (value) => value.column,
       join: (value) {
-        return '${value.key ?? value.table.tableName}${value.candidateKey.isNullOrBlank ? ":${value.foreignKey}" : ""}${value.candidateKey.isNotNullOrEmpty ? "!${value.candidateKey}" : ""}(${value.table.selectStatement})';
+        return '${value.key ?? value.table.tableName}${value.candidateKey.isNullOrBlank ? ":${value.foreignKey}" : ""}${value.candidateKey.isNotNullOrEmpty ? ":${value.table.tableName}!${value.candidateKey}" : ""}(${value.table.selectStatement})';
       },
     );
   }
