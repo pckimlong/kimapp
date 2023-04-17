b0a877fb677f1478616e09abcabe35bc3d63de48
Kim <78638472+pckimlong@users.noreply.github.com>
2023-04-17 15:55:40 +0700

    use constructor params

--- packages/kimapp_generator/lib/src/table_model_generator.dart
+++ packages/kimapp_generator/lib/src/table_model_generator.dart
@@ -4,8 +4,6 @@ import 'package:build/build.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:kimapp/kimapp.dart';
 import 'package:source_gen/source_gen.dart';
-import 'package:analyzer/src/dart/element/inheritance_manager3.dart' show InheritanceManager3;
-import 'package:collection/collection.dart';
 
 class TableModelGenerator extends GeneratorForAnnotation<TableModel> {
   @override
@@ -26,10 +24,9 @@ class TableModelGenerator extends GeneratorForAnnotation<TableModel> {
       throw 'Default constructor for ${element.name} is missing';
     }
 
-    final allField = constructor.parameters.map((e) => e as FieldElement);
     final fields = <TableFieldInfo>[];
 
-    for (final fieldElement in allField) {
+    for (final fieldElement in constructor.parameters) {
       String? key;
       String? candidateKey;
       String? foreignKey;
@@ -42,8 +39,7 @@ class TableModelGenerator extends GeneratorForAnnotation<TableModel> {
 
       const checker = TypeChecker.fromRuntime(JsonKey);
 
-      final annotation = checker.firstAnnotationOf(fieldElement) ??
-          (fieldElement.getter == null ? null : checker.firstAnnotationOf(fieldElement.getter!));
+      final annotation = checker.firstAnnotationOf(fieldElement);
 
       final reader = ConstantReader(annotation);
 
@@ -54,63 +50,18 @@ class TableModelGenerator extends GeneratorForAnnotation<TableModel> {
       key = reader.peek('name')?.stringValue;
 
       // If the field is a list, we add more algorithm to check if it is a joined column
-      if (fieldElement.type.isDartCoreList) {
-        // Check if it has metadata
-        if (fieldElement.metadata.isNotEmpty) {
-          // Check if it has JoinedColumn
-          if (_fieldHasAnnotation(JoinedColumn, fieldElement)) {
-            const checker = TypeChecker.fromRuntime(JoinedColumn);
-            final annotation = checker.firstAnnotationOf(fieldElement) ??
-                (fieldElement.getter == null
-                    ? null
-                    : checker.firstAnnotationOf(fieldElement.getter!));
-            final reader = ConstantReader(annotation);
-            candidateKey = reader.peek('candidateKey')?.stringValue;
-            foreignKey = reader.peek('foreignKey')?.stringValue;
-            var elementType = fieldElement.type as ParameterizedType;
-            var listType = elementType.typeArguments[0].getDisplayString(withNullability: true);
-            joinedModel = listType;
-          }
+      if (_fieldHasAnnotation(JoinedColumn, fieldElement)) {
+        const checker = TypeChecker.fromRuntime(JoinedColumn);
+        final annotation = checker.firstAnnotationOf(fieldElement);
+        final reader = ConstantReader(annotation);
+        candidateKey = reader.peek('candidateKey')?.stringValue;
+        foreignKey = reader.peek('foreignKey')?.stringValue;
+        if (fieldElement.type.isDartCoreList) {
+          var elementType = fieldElement.type as ParameterizedType;
+          var listType = elementType.typeArguments[0].toString();
+          joinedModel = listType;
         } else {
-          // If it doesn't have metadata, then it might be a list of freezed model
-          // Use freezed workaround to get the type
-          // find private field with the same name as the list
-          final privateField =
-              allField.firstWhereOrNull((e) => e.name == "_${fieldElement.name}" && e.isPrivate);
-
-          // If founded then check if it has JoinedColumn
-          if (privateField != null && _fieldHasAnnotation(JoinedColumn, privateField)) {
-            const checker = TypeChecker.fromRuntime(JoinedColumn);
-            final annotation = checker.firstAnnotationOf(privateField) ??
-                (privateField.getter == null
-                    ? null
-                    : checker.firstAnnotationOf(privateField.getter!));
-            final reader = ConstantReader(annotation);
-            candidateKey = reader.peek('candidateKey')?.stringValue;
-            foreignKey = reader.peek('foreignKey')?.stringValue;
-            var elementType = privateField.type as ParameterizedType;
-            var listType = elementType.typeArguments[0].getDisplayString(withNullability: true);
-            joinedModel = listType;
-          }
-        }
-      } else {
-        // Not a list just check if it has JoinedColumn
-        if (_fieldHasAnnotation(JoinedColumn, fieldElement)) {
-          const checker = TypeChecker.fromRuntime(JoinedColumn);
-          final annotation = checker.firstAnnotationOf(fieldElement) ??
-              (fieldElement.getter == null
-                  ? null
-                  : checker.firstAnnotationOf(fieldElement.getter!));
-          final reader = ConstantReader(annotation);
-          candidateKey = reader.peek('candidateKey')?.stringValue;
-          foreignKey = reader.peek('foreignKey')?.stringValue;
-          if (fieldElement.type.isDartCoreList) {
-            var elementType = fieldElement.type as ParameterizedType;
-            var listType = elementType.typeArguments[0].getDisplayString(withNullability: true);
-            joinedModel = listType;
-          } else {
-            joinedModel = fieldElement.type.toString();
-          }
+          joinedModel = fieldElement.type.toString();
         }
       }
 
@@ -150,7 +101,7 @@ class TableModelGenerator extends GeneratorForAnnotation<TableModel> {
   }
 }
 
-bool _fieldHasAnnotation(Type annotationType, FieldElement element) {
+bool _fieldHasAnnotation(Type annotationType, ParameterElement element) {
   final annotations = TypeChecker.fromRuntime(annotationType).annotationsOf(element);
   return annotations.isNotEmpty;
 }
@@ -166,31 +117,4 @@ class TableFieldInfo {
     this.foreignKey,
     this.joinedModel,
   });
-}
-
-List<FieldElement> _getAllField(ClassElement element) {
-  List<FieldElement> fieldElements = [];
-
-  /// Since how freezed treat with list field, I need to allow private field if it is a list
-  fieldElements
-      .addAll(element.fields.where((e) => !e.isStatic && (!e.isPrivate || e.type.isDartCoreList)));
-
-  final manager = InheritanceManager3();
-
-  for (final v in manager.getInheritedConcreteMap2(element).values) {
-    assert(v is! FieldElement);
-    const checker = TypeChecker.fromRuntime(Object);
-
-    if (checker.isExactly(v.enclosingElement)) {
-      continue;
-    }
-
-    if (v is PropertyAccessorElement && v.isGetter) {
-      final variable = v.variable as FieldElement;
-      assert(!fieldElements.contains(variable));
-      fieldElements.add(variable);
-    }
-  }
-
-  return fieldElements;
 }
