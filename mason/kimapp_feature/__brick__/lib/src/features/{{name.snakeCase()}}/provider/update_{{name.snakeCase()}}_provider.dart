0877409da4f563fdbdd6962b6d43fba9d4a3b274
Kim <78638472+pckimlong@users.noreply.github.com>
2023-04-26 22:32:56 +0700

    Revert "no detail"
    This reverts commit 143c2cd2fb4f4a1d0ecfaa5c8e9295aa04e9ba7e.

--- mason/kimapp_feature/__brick__/lib/src/features/{{name.snakeCase()}}/provider/update_{{name.snakeCase()}}_provider.dart
+++ mason/kimapp_feature/__brick__/lib/src/features/{{name.snakeCase()}}/provider/update_{{name.snakeCase()}}_provider.dart
@@ -45,7 +45,7 @@ class Update{{name.pascalCase()}} extends _$Update{{name.pascalCase()}} with _$U
         return result.getOrThrow();
       },
       onSuccess: (success) {
-        ref.read({{name.camelCase()}}ListProvider.notifier).updateItem(success);
+        ref.read({{name.camelCase()}}ListProvider.notifier).updateItem({{name.pascalCase()}}Model.fromDetailModel(success));
         ref.read({{name.camelCase()}}DetailProvider(FindOne{{name.pascalCase()}}Param.byId({{name.camelCase()}}Id)).notifier).updateState((_) => success);
         ref.invalidate({{name.camelCase()}}ListPaginationProvider);
       },
