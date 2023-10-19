6807e9b829fc33ccffdb016754f230c6ab4ee077
Kim <78638472+pckimlong@users.noreply.github.com>
2023-10-19 11:42:27 +0700

    improve mason ui

--- mason/kimapp/__brick__/lib/src/presentation/app/app_widget.dart
+++ mason/kimapp/__brick__/lib/src/presentation/app/app_widget.dart
@@ -3,6 +3,20 @@ import 'app_theme_provider.dart';
 import '../router/app_router_provider.dart';
 import '../../../exports.dart';
 
+// Response for initialize app, and other global state
+class _EagerInitialization extends ConsumerWidget {
+  const _EagerInitialization({required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Initialize app state
+    ref.watch(appStateProvider);
+
+    return child;
+  }
+}
+
 class AppWidget extends HookConsumerWidget {
   const AppWidget({super.key});
 
@@ -10,37 +24,39 @@ class AppWidget extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(appRouterProvider);
 
-    return MaterialApp.router(
-      title: Config.appName,
-      color: Colors.blue,
-      theme: ref.watch(themeProvider),
-      themeMode: ref.watch(themeModeProvider).valueOrNull,
-      debugShowCheckedModeBanner: false,
-      routeInformationParser: router.defaultRouteParser(),
-      routerDelegate: router.delegate(),
-       localizationsDelegates: [
-        ...context.localizationDelegates,
-        OverrideFormBuilderLocalizationsEn.delegate
-      ],
-      supportedLocales: [
-        ...context.supportedLocales,
-        ...FormBuilderLocalizations.supportedLocales,
-      ],
-      locale: context.locale,
-      builder: (context, child) {
-        child = BotToastInit()(context, child);
-        child = _Unfocus(child: child);
-        child = ResponsiveBreakpoints.builder(
-          child: child,
-          breakpoints: [
-            const Breakpoint(start: 0, end: AS.mobileBreakpoint, name: MOBILE),
-            const Breakpoint(start: AS.mobileBreakpoint, end: AS.tabletBreakpoint, name: TABLET),
-            const Breakpoint(start: AS.desktopBreakpoint, end: 1920, name: DESKTOP),
-            const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
-          ],
-        );
-        return child;
-      },
+    return _EagerInitialization(
+        child: MaterialApp.router(
+        title: Config.appName,
+        color: Colors.blue,
+        theme: ref.watch(themeProvider),
+        themeMode: ref.watch(themeModeProvider).valueOrNull,
+        debugShowCheckedModeBanner: false,
+        routeInformationParser: router.defaultRouteParser(),
+        routerDelegate: router.delegate(),
+        localizationsDelegates: [
+          ...context.localizationDelegates,
+          OverrideFormBuilderLocalizationsEn.delegate
+        ],
+        supportedLocales: [
+          ...context.supportedLocales,
+          ...FormBuilderLocalizations.supportedLocales,
+        ],
+        locale: context.locale,
+        builder: (context, child) {
+          child = BotToastInit()(context, child);
+          child = _Unfocus(child: child);
+          child = ResponsiveBreakpoints.builder(
+            child: child,
+            breakpoints: [
+              const Breakpoint(start: 0, end: AS.mobileBreakpoint, name: MOBILE),
+              const Breakpoint(start: AS.mobileBreakpoint, end: AS.tabletBreakpoint, name: TABLET),
+              const Breakpoint(start: AS.desktopBreakpoint, end: 1920, name: DESKTOP),
+              const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+            ],
+          );
+          return child;
+        },
+      ),
     );
   }
 }
