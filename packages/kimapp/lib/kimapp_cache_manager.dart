8b21c4c99509b2007b121e2ea6e0d76f343fdac7
pckimlong <pochkimlong88@gmail.com>
2024-01-31 22:06:20 +0700

    feat: nullable savable

--- packages/kimapp/lib/kimapp_cache_manager.dart
+++ packages/kimapp/lib/kimapp_cache_manager.dart
@@ -4,24 +4,24 @@ abstract class KimappCacheManager {
   Future<void> save(String key, dynamic value);
   Future<dynamic> read(String key);
 
-  Future<void> saveString(String key, String value);
+  Future<void> saveString(String key, String? value);
   Future<String?> readString(String key);
 
-  Future<void> saveInt(String key, int value);
+  Future<void> saveInt(String key, int? value);
   Future<int?> readInt(String key);
 
-  Future<void> saveDouble(String key, double value);
+  Future<void> saveDouble(String key, double? value);
   Future<double?> readDouble(String key);
 
-  Future<void> saveMap(String key, Map<String, dynamic> value);
+  Future<void> saveMap(String key, Map<String, dynamic>? value);
   Future<Map<String, dynamic>?> readMap(String key);
 
-  Future<void> saveEnum<T extends Enum>(String key, T value);
+  Future<void> saveEnum<T extends Enum>(String key, T? value);
   Future<T?> readEnum<T extends Enum>(String key, T Function(String name) parser);
 
   Future<void> saveObject<T extends Object>(
     String key, {
-    required T value,
+    required T? value,
     required Map<String, dynamic> Function(T object) toMap,
   });
 
@@ -33,17 +33,17 @@ abstract class KimappCacheManager {
 
   Future<void> saveList<T extends Object>(
     String key, {
-    required List<T> value,
+    required List<T?> value,
     required Map<String, dynamic> Function(T object) toMap,
   });
 
-  Future<List<T>?> readList<T extends Object>(
+  Future<List<T?>?> readList<T extends Object>(
     String key, {
     required T Function(Map<String, dynamic> json) fromMap,
     void Function(Object error, Map<String, dynamic> json)? onFail,
   });
 
-  Future<void> saveDateTime(String key, DateTime dateTime);
+  Future<void> saveDateTime(String key, DateTime? dateTime);
   Future<DateTime?> readDateTime(String key);
 
   Future<void> clear(String key);
