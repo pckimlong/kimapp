56d68dd18e975a6fd8001be0efff4e2d81fe2091
pckimlong <pochkimlong88@gmail.com>
2024-03-13 08:21:41 +0700

    Update dependencies and fix code issues

--- mason/kimapp/__brick__/lib/src/commons/file/file_from_url_provider.dart
+++ mason/kimapp/__brick__/lib/src/commons/file/file_from_url_provider.dart
@@ -1,15 +1,16 @@
 import 'dart:io';
-import 'package:{{project_name.snakeCase()}}/exports.dart';
+
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:{{project_name.snakeCase()}}/exports.dart';
 
 part "file_from_url_provider.g.dart";
 
 /// Provide use to get uint8list from url and also cache it
 @riverpod
 FutureOr<File?> fileFromUrl(FileFromUrlRef ref, String? url) async {
-  ref.catchTime(const Duration(minutes: 3));
+  ref.cacheTime(const Duration(minutes: 3));
 
   if (url == null) return null;
   final path = await ref.watch(_tempDirProvider.future);
