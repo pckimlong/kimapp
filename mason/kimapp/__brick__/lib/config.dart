12caefebef3154ef912a8d6a9bf394006d36355b
pckimlong <pochkimlong88@gmail.com>
2024-05-15 08:26:17 +0700

    bump dependencies

--- mason/kimapp/__brick__/lib/config.dart
+++ mason/kimapp/__brick__/lib/config.dart
@@ -2,4 +2,10 @@ class Config {
   const Config._();
 
   static const appName = "{{project_name.sentenceCase()}}";
+
+  /// Enable riverpod observers
+  static const bool logRiverpod = true;
+
+  /// Enable riverpod observers on error state
+  static const bool logRiverpodError = true;
 }
