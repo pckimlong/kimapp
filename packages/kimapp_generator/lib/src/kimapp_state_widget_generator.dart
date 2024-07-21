7d8322e58451be225eb917ed6bc610cf92bfb389
pckimlong <pochkimlong88@gmail.com>
2024-07-21 16:15:10 +0700

    add state annotation

---
+++ packages/kimapp_generator/lib/src/kimapp_state_widget_generator.dart
@@ -0,0 +1,15 @@
+
+import 'package:analyzer/dart/element/element.dart';
+import 'package:build/src/builder/build_step.dart';
+import 'package:kimapp/kimapp.dart';
+import 'package:source_gen/source_gen.dart';
+
+class KimappStateWidgetGenerator extends GeneratorForAnnotation<StateWidget> {
+  @override
+  generateForAnnotatedElement(Element element, ConstantReader annotation, BuildStep buildStep) {
+    // TODO: implement generateForAnnotatedElement
+    throw UnimplementedError();
+  }
+  
+
+}
