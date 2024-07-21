1e3789606001f2a8749b993eb999d5747025b1f0
pckimlong <pochkimlong88@gmail.com>
2024-07-21 21:22:54 +0700

    add state widget generator

--- packages/kimapp_generator/lib/builder.dart
+++ packages/kimapp_generator/lib/builder.dart
@@ -4,6 +4,8 @@ import 'package:kimapp_generator/src/table_model_generator.dart';
 import 'package:kimapp_generator/src/table_structure_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
+import 'src/kimapp_state_widget_generator.dart';
+
 /// Create separate generator because I need to make this table run before json serializable
 Builder kimappTableGenerator(BuilderOptions options) =>
     LibraryBuilder(TableStructureGenerator(), generatedExtension: '.table.dart');
