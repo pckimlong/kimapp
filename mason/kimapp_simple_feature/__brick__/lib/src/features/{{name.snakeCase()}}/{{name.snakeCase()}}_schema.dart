786a14473cd3f0dc0dcbbd2eb67f40f2d7200700
pckimlong <pochkimlong88@gmail.com>
2024-07-24 13:15:20 +0700

    fix join column key

--- mason/kimapp_simple_feature/__brick__/lib/src/features/{{name.snakeCase()}}/{{name.snakeCase()}}_schema.dart
+++ mason/kimapp_simple_feature/__brick__/lib/src/features/{{name.snakeCase()}}/{{name.snakeCase()}}_schema.dart
@@ -4,23 +4,34 @@ import '{{name.snakeCase()}}_schema.schema.dart';
 
 export '{{name.snakeCase()}}_schema.schema.dart';
 
+const _create{{name.pascalCase()}}Param = 'Create{{name.pascalCase()}}Param';
+const _update{{name.pascalCase()}}Param = 'Update{{name.pascalCase()}}Param';
+
 @KimappSchema(
   tableName: '{{name.snakeCase()}}s',
   className: '{{name.pascalCase()}}',
-  columns: [
-    IdField('id', generateAs: '{{name.pascalCase()}}Id', type: {{id_data_type}}),
+  fields: [
+    IdField(
+      'id',
+      generateAs: '{{name.pascalCase()}}Id',
+      type: T(int),
+      addToModels: [
+        _create{{name.pascalCase()}}Param,
+        _update{{name.pascalCase()}}Param,
+      ],
+    ),
     Field(
       'name',
-      type: String,
+      type: T(String),
       addToModels: [
-        'Create{{name.pascalCase()}}Param',
-        'Update{{name.pascalCase()}}Param',
+        _create{{name.pascalCase()}}Param,
+        _update{{name.pascalCase()}}Param,
       ],
     ),
   ],
   models: [
-    Model(name: 'Create{{name.pascalCase()}}Param'),
-    Model(name: 'Update{{name.pascalCase()}}Param'),
+    Model(name: _create{{name.pascalCase()}}Param),
+    Model(name: _update{{name.pascalCase()}}Param),
   ],
 )
 typedef {{name.pascalCase()}}Model = {{name.pascalCase()}}RawModel;
