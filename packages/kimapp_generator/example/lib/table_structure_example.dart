dc97b92ff895224ab90676ea7e7c8831a09a6fb0
pckimlong <pochkimlong88@gmail.com>
2024-07-24 15:30:37 +0700

    fix table structure generate when no field

--- packages/kimapp_generator/example/lib/table_structure_example.dart
+++ packages/kimapp_generator/example/lib/table_structure_example.dart
@@ -1,27 +1,15 @@
 
-// import 'package:example/state_widget_example.dart';
-// import 'package:kimapp/annotation.dart';
+import 'package:kimapp/annotation.dart';
 
-// @TableStructure(
-//   'samples',
-//   classPrefixName: 'Sample',
-//   idColumn: 'SampleId:int',
-//   additionalClasses: [
-//     'UpdateSampleParam:table(v_sample)[address:int, ex:ExampleModel(example_id)]',
-//   ],
-//   columns: [
-//     'id:SampleId[0]',
-//     'name:String[0,1]',
-//     'description:String?[0,1]',
-//     'createdAt:DateTime[1]',
-//     'updatedAt:DateTime[0,1]',
-//     'ids:List<int>[0,1]',
-//     'good',
-//   ],
-//   generateRawClass: true,
-//   rawClassTableMode: true,
-//   customTypes: [
-//     ExampleModel,
-//   ],
-// )
-// const placeHolder = 'It can be anything!'; 
+@TableStructure(
+  'samples',
+  classPrefixName: 'Sample',
+  idColumn: 'SampleId:int',
+  columns: [
+    'id',
+    'name',
+    'description',
+  ],
+
+)
+const placeHolder = 'It can be anything!'; 
