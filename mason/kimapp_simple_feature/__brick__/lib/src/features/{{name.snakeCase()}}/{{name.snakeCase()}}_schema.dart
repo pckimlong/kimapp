25068ab5fa8a9ce44fedf3dab586acd1597d70fd
pckimlong <pochkimlong88@gmail.com>
2024-07-26 20:04:38 +0700

    kimapp_schema_generator implementation

--- mason/kimapp_simple_feature/__brick__/lib/src/features/{{name.snakeCase()}}/{{name.snakeCase()}}_schema.dart
+++ mason/kimapp_simple_feature/__brick__/lib/src/features/{{name.snakeCase()}}/{{name.snakeCase()}}_schema.dart
@@ -2,35 +2,25 @@ import 'package:kimapp/kimapp.dart';
 
 export '{{name.snakeCase()}}_schema.schema.dart';
 
-const _create{{name.pascalCase()}}Param = 'Create{{name.pascalCase()}}Param';
-const _update{{name.pascalCase()}}Param = 'Update{{name.pascalCase()}}Param';
+const _create = 'Create{{name.pascalCase()}}Param';
+const _update = 'Update{{name.pascalCase()}}Param';
+const _lite = '{{name.pascalCase()}}LiteModel';
 
 @KimappSchema(
   tableName: '{{name.snakeCase()}}s',
   className: '{{name.pascalCase()}}',
   baseModelName: '{{name.pascalCase()}}Model',
   fields: [
-    IdField(
-      'id',
-      generateAs: '{{name.pascalCase()}}Id',
-      type: T(int),
-      addToModels: [
-        _create{{name.pascalCase()}}Param,
-        _update{{name.pascalCase()}}Param,
-      ],
-    ),
-    Field(
-      'name',
-      type: T(String),
-      addToModels: [
-        _create{{name.pascalCase()}}Param,
-        _update{{name.pascalCase()}}Param,
-      ],
-    ),
+    IdField('id', generateAs: '{{name.pascalCase()}}Id', type: T({{id_data_type}}), addToModels: [_create, _update, _lite]),
+    Field('name', type: T(String), addToModels: [_create, _update, _lite]),
   ],
   models: [
-    Model(name: _create{{name.pascalCase()}}Param),
-    Model(name: _update{{name.pascalCase()}}Param),
+    Model(name: _create),
+    Model(name: _update),
+    Model(name: _lite),
   ],
 )
 abstract class {{name.pascalCase()}}Schema {}
+
+
+
