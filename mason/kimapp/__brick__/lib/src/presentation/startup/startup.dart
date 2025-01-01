7b7ba265fbe8c067c5dc4b5206b6c54feba9cee5
pckimlong <pochkimlong88@gmail.com>
2025-01-01 14:59:22 +0700

    mason

--- mason/kimapp/__brick__/lib/src/presentation/startup/startup.dart
+++ mason/kimapp/__brick__/lib/src/presentation/startup/startup.dart
@@ -1,7 +1,5 @@
-import 'dart:ffi';
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 import '../../../exports.dart';
@@ -23,7 +21,7 @@ class KimappRunner {
     required IntegrationMode env,
     LaunchConfiguration config = const LaunchConfiguration(),
   }) async {
-    _setIntegrationMode(env);
+    IntegrationMode.setMode(env);
     WidgetsFlutterBinding.ensureInitialized();
 
     final container = ProviderContainer(
@@ -97,91 +95,4 @@ class LaunchContext {
   ProviderContainer container;
   IntegrationMode env;
   PlatformType platform;
-}
-
-enum IntegrationMode {
-  develop,
-  release,
-  unitTest,
-  integrationTest;
-
-  bool get isTest => isUnitTest || isIntegrationTest;
-  bool get isUnitTest => this == IntegrationMode.unitTest;
-  bool get isIntegrationTest => this == IntegrationMode.integrationTest;
-  bool get isRelease => this == IntegrationMode.release;
-  bool get isDevelop => this == IntegrationMode.develop;
-}
-
-void _setIntegrationMode(IntegrationMode mode) {
-  _integrationMode = mode;
-}
-
-// User defined integration mode, if not set, it will be determined by the platform
-IntegrationMode? _integrationMode;
-IntegrationMode get integrationMode {
-  if (_integrationMode != null) {
-    return _integrationMode!;
-  }
-
-  if (platformType.isWeb) {
-    if (kDebugMode) {
-      return IntegrationMode.develop;
-    }
-
-    return IntegrationMode.release;
-  } else {
-    if (Platform.environment.containsKey('FLUTTER_TEST')) {
-      return IntegrationMode.unitTest;
-    }
-
-    if (kReleaseMode) {
-      return IntegrationMode.release;
-    }
-
-    return IntegrationMode.develop;
-  }
-}
-
-enum PlatformType {
-  android,
-  ios,
-  web,
-  macos,
-  windows,
-  linux;
-
-  bool get isAndroid => this == PlatformType.android;
-  bool get isIos => this == PlatformType.ios;
-  bool get isWeb => this == PlatformType.web;
-  bool get isMacos => this == PlatformType.macos;
-  bool get isWindows => this == PlatformType.windows;
-  bool get isLinux => this == PlatformType.linux;
-}
-
-PlatformType get platformType {
-  if (kIsWeb) {
-    return PlatformType.web;
-  } else {
-    if (Platform.isAndroid) {
-      return PlatformType.android;
-    }
-
-    if (Platform.isIOS) {
-      return PlatformType.ios;
-    }
-
-    if (Platform.isMacOS) {
-      return PlatformType.macos;
-    }
-
-    if (Platform.isWindows) {
-      return PlatformType.windows;
-    }
-
-    if (Platform.isLinux) {
-      return PlatformType.linux;
-    }
-  }
-
-  throw UnsupportedError('Unsupported platform');
 }
