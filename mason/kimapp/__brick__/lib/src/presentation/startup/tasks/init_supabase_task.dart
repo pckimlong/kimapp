720dd520dcca88b9579f5f9638ff4b0c43a8ad27
pckimlong <pochkimlong88@gmail.com>
2025-02-06 11:35:26 +0700

    add stage mode

--- mason/kimapp/__brick__/lib/src/presentation/startup/tasks/init_supabase_task.dart
+++ mason/kimapp/__brick__/lib/src/presentation/startup/tasks/init_supabase_task.dart
@@ -2,7 +2,7 @@ import 'package:kimapp_supabase_helper/kimapp_supabase_helper.dart';
 
 import '../startup.dart';
 
-const _devUrl = "http://192.168.0.199:54321";
+final _devUrl = platformType.isAndroid ? "http://10.0.2.2:54321" : "http://localhost:54321";
 const _devAnonKey =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
 
