e441a5b941e1829b69174d51168c8f981969f144
pckimlong <pochkimlong88@gmail.com>
2025-02-13 10:09:41 +0700

    improve kimapp generator form

--- packages/kimapp_generator/example/lib/demo_schema.dart
+++ packages/kimapp_generator/example/lib/demo_schema.dart
@@ -7,6 +7,7 @@ class Demo extends KimappSchema {
   final name = Field<String?>('name');
   final description = Field<String?>('description');
   final age = Field<int>('age');
+  final birthdate = Field<DateTime?>('birthdate');
   final address = Field<String?>('address');
   final other = Field.join<OtherModel?>().withForeignKey('other_id');
 
