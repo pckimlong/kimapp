cd5e8a8fbaea26e177c5bb95c2ce8a7b51ad97bf
pckimlong <pochkimlong88@gmail.com>
2025-02-24 10:12:03 +0700

    feat: add widget helper and update state widget generation
    - Introduced WidgetExtension with dialog and bottom sheet helper methods
    - Updated state widget generator to improve parameter handling
    - Added @stateWidget annotation to list and detail providers
    - Removed generated freezed file for theme extension
    - Refined generator comments and parameter naming

--- mason/kimapp_simple_feature/__brick__/lib/src/features/{{name.snakeCase()}}/providers/{{name.snakeCase()}}_detail_provider.dart
+++ mason/kimapp_simple_feature/__brick__/lib/src/features/{{name.snakeCase()}}/providers/{{name.snakeCase()}}_detail_provider.dart
@@ -1,13 +1,15 @@
-
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kimapp/kimapp.dart';
+import 'package:kimapp_utils/kimapp_utils.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-
 
 import '../{{name.snakeCase()}}_schema.schema.dart';
 import '../i_{{name.snakeCase()}}_repo.dart';
 
 part '{{name.snakeCase()}}_detail_provider.g.dart';
 
+@stateWidget
 @riverpod
 class {{name.pascalCase()}}Detail extends _${{name.pascalCase()}}Detail {
   @override
