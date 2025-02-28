0c036bf0571f5cab5fe11b50e3c44790f194008b
pckimlong <pochkimlong88@gmail.com>
2025-02-28 08:25:40 +0700

    restructure, and set up form generator

---
+++ pubs/riverpod_widget/lib/src/generators/state_widget/widgets/scope_widget.dart
@@ -0,0 +1,88 @@
+import 'package:riverpod_widget/src/models/provider_definition.dart';
+import 'package:riverpod_widget/src/templates/consumer_widget.dart';
+import 'package:riverpod_widget/src/templates/utils.dart';
+
+import '../state_widget_names.dart';
+
+String generateScopeWidget(ProviderDefinition provider) {
+  final isAsyncValue = provider.isAsyncValue;
+  final stateType = provider.getProviderType(name: isAsyncValue ? 'asyncValue' : 'state');
+  final baseReturnType = provider.returnType.baseType;
+  final builderType =
+      "Widget Function(BuildContext context, ${provider.proxyWidgetName} ref, $stateType, Widget? child,)?";
+  final hasFamily = provider.familyParameters.isNotEmpty;
+
+  String inheritedWidget({required String child}) {
+    if (!hasFamily) return child;
+
+    return '''
+    ${provider.paramInheritedWidgetName}(
+      params: ${provider.familyAsRecordBindString()},
+      child: $child,
+    )
+    ''';
+  }
+
+  String asyncTypeBuilder() {
+    if (!isAsyncValue) return 'return child ?? const SizedBox.shrink();';
+
+    return '''
+              final themeExtension = Theme.of(context).extension<KimappThemeExtension>();
+              return state.when(
+                data: (data) {
+                  final result = this.data?.call(data) ?? child;
+                  if (result == null) {
+                    debugPrint('No child provided for ${provider.baseName}ProviderScope. Empty SizedBox will be returned.');
+                    return const SizedBox.shrink();
+                  }
+                  return result;
+                },
+                error: (error, stack) =>
+                    this.error?.call(error, stack) ?? 
+                    themeExtension?.defaultErrorStateWidget?.call(context, ref, error) ??
+                    const SizedBox.shrink(),
+                loading: () => loading?.call() ?? 
+                    themeExtension?.defaultLoadingStateWidget?.call(context, ref) ??
+                    const SizedBox.shrink(),
+              );
+    ''';
+  }
+
+  return generateConsumerWidget(
+    name: provider.providerScopeName,
+    fields: [
+      ...provider.familyParameters.map((p) => p.toClassField()),
+      if (isAsyncValue) ...[
+        ClassField(name: 'loading', type: 'Widget Function()?', isRequired: false),
+        ClassField(
+          name: 'error',
+          type: 'Widget Function(Object error, StackTrace? stackTrace)?',
+          isRequired: false,
+        ),
+        ClassField(name: 'data', type: 'Widget Function($baseReturnType data)?', isRequired: false),
+      ],
+      ClassField(name: 'builder', type: builderType, isRequired: false),
+      ClassField(name: 'child', type: 'Widget?', isRequired: false),
+      ClassField.providerListenable('onStateChanged', provider.getProviderType()),
+    ],
+    build: '''
+if (onStateChanged != null) {
+  ref.listen(${provider.providerNameWithFamily()}, onStateChanged!);
+}
+
+return ${inheritedWidget(child: '''
+ Consumer(
+  builder: (context, ref, child) {
+    final state = ref.watch(${provider.providerNameWithFamily()});
+
+    if (builder != null) {
+      return builder!(context, ${provider.proxyWidgetName}(ref), state, child);
+    }
+    
+    ${asyncTypeBuilder()}
+  },
+ )
+''')};
+''',
+  );
+}
