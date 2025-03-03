55360eed86c61936c8895d42a480f8aa09c0a9a4
pckimlong <pochkimlong88@gmail.com>
2025-03-03 23:25:23 +0700

    Enhance generator imports and field type handling
    - Add support for collecting additional imports in GeneratorRegistry
    - Improve field type import detection in form widget generator
    - Update FieldDefinition to track import paths for custom types
    - Refactor import handling in UnifiedWidgetBuilder
    - Simplify import collection across generator components

--- pubs/autoverpod_generator/lib/src/core/generator_registry.dart
+++ pubs/autoverpod_generator/lib/src/core/generator_registry.dart
@@ -1,5 +1,5 @@
-import 'package:build/build.dart';
 import 'package:autoverpod_generator/src/core/generator.dart';
+import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
 /// Registry for all widget generators
@@ -14,27 +14,38 @@ class GeneratorRegistry {
   /// List of registered generators
   final List<WidgetGenerator> _generators = [];
 
+  /// Set of additional imports required by generators
+  final Set<String> _additionalImports = {};
+
   /// Register a new generator
-  void registerGenerator(WidgetGenerator generator) =>
-      _generators.add(generator);
+  void registerGenerator(WidgetGenerator generator) => _generators.add(generator);
 
   /// Get all registered generators
   List<WidgetGenerator> get generators => List.unmodifiable(_generators);
+
+  /// Add an additional import that will be included in generated files
+  void addImport(String import) => _additionalImports.add(import);
 
   /// Get all required imports from all registered generators
   List<String> getAllRequiredImports() {
     final imports = <String>{};
 
+    // Add imports from generators
     for (final generator in _generators) {
       imports.addAll(generator.getRequiredImports());
     }
+
+    // Add additional imports
+    imports.addAll(_additionalImports);
 
     return imports.toList();
   }
 
   /// Generate code for all registered generators
   Future<String> generateForLibrary(
-      LibraryReader library, BuildStep buildStep) async {
+    LibraryReader library,
+    BuildStep buildStep,
+  ) async {
     final buffer = StringBuffer();
 
     for (final generator in _generators) {
