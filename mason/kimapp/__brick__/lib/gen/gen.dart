4a3463d561a6381c308dfbc610e60fb4be2789bb
pckimlong <pochkimlong88@gmail.com>
2025-03-15 19:57:27 +0700

    Remove files that should be ignored and update .gitignore tracking

--- mason/kimapp/__brick__/lib/gen/gen.dart
+++
@@ -1,3 +0,0 @@
-
-//* Run this script in terminal to generate locale keys
-// flutter pub run easy_localization:generate -f keys -o locale_keys.g.dart --source-dir assets/translations --output-dir lib/gen
