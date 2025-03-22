701f0f81ec58ebe01e22c6b0562df95d851e8b61
pckimlong <pochkimlong88@gmail.com>
2025-03-22 11:26:54 +0700

    improve template

--- template_generators/kimapp_feature/src/generators/providers/update.js
+++ template_generators/kimapp_feature/src/generators/providers/update.js
@@ -50,8 +50,6 @@ class ${pascalCaseName}Update extends _$${pascalCaseName}UpdateWidget {
     /// in this case, the item should be removed from the paginated list, but using this method will just update the item
     /// other case is if we update sort order, the item might need to change position
     ${pascalCaseName}PaginationTracker.instance.updatePaginatedItem(ref, result);
-    
-    super.onSuccess(result);
   }
 }`;
 
